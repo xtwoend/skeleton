39efc717e5f50e7b4ae4b0b251845f9011ff0356
Abdul Hafidz Anshari <aditans88@gmail.com>
2020-11-16 22:27:08 +0700

    add test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace :uc:vendor\:uc:package\Tests;
+
+use :uc:vendor\:uc:package\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->get('/');
+
+        $this->assertEquals(
+            $this->app->version(), $this->response->getContent()
+        );
+    }
+}
